1189bf3324869081610cac0da047be42580a2cae
crisc <crisc@LAPTOP-M1HA1MO3>
2023-10-22 12:40:03 +0200

    Bug Fixed

--- valutatore-bello/src/valutatori/LettereExpVisitor.java
+++ valutatore-bello/src/valutatori/LettereExpVisitor.java
@@ -2,22 +2,52 @@ package valutatori;
 
 import albero.*;
 import linguaggi.*;
+import pl.allegro.finance.tradukisto.ValueConverters;
 
-public class LettereExpVisitor implements ExpVisitor{
+public class PostExpVisitor implements ExpVisitor{
 	
 
 	String curs = "";
 	public String getResult() { return curs; } // accessor
+	
 	protected void visitOpExp(OpExp e){
 	e.left().accept(this); String sleft = getResult();
 	e.right().accept(this); String sright = getResult();
-	curs = "(" + sleft + " " + sright + " " +  e.myOp() + " " + ")";
+	String op="";
+	switch( e.myOp()) {
+		case "+" :
+			op = "più";
+			break;
+		case "-" :
+			op = "meno";
+			break;
+		case "*" :
+			op = "per";
+			break;
+		case "/" :
+			op = "diviso";
+			break;
+		case "^" :
+			op = "alla";
+			break;
+		default:
+			op="OPERATORE NON RICONOSCIUTO";
 	}
+	curs = "(" + sleft + " "  +  op + " " + sright + " "+ ")";
+	}
+	
+	
 	public void visit( PlusExp e ) { visitOpExp(e); }
 	public void visit( MinusExp e ) { visitOpExp(e); }
 	public void visit( TimesExp e ) { visitOpExp(e); }
 	public void visit( DivExp e ) { visitOpExp(e); }
 	public void visit( PotExp e ) { visitOpExp(e); }
-	public void visit( NumExp e ) { curs = "" + e.getValue(); }
+	
+	public void visit( NumExp e ) { 
+		ValueConverters intConverter = ValueConverters.ITALIAN_INTEGER;
+		String num = intConverter.asWords(e.getValue());
+		curs = "" + num; 
+	
+	}
 	
 }
--- valutatore-bello/src/valutatori/PostExpVisitor.java
+++ valutatore-bello/src/valutatori/PostExpVisitor.java
@@ -2,52 +2,22 @@ package valutatori;
 
 import albero.*;
 import linguaggi.*;
-import pl.allegro.finance.tradukisto.ValueConverters;
 
-public class PostExpVisitor implements ExpVisitor{
+public class LettereExpVisitor implements ExpVisitor{
 	
 
 	String curs = "";
 	public String getResult() { return curs; } // accessor
-	
 	protected void visitOpExp(OpExp e){
 	e.left().accept(this); String sleft = getResult();
 	e.right().accept(this); String sright = getResult();
-	String op="";
-	switch( e.myOp()) {
-		case "+" :
-			op = "più";
-			break;
-		case "-" :
-			op = "meno";
-			break;
-		case "*" :
-			op = "per";
-			break;
-		case "/" :
-			op = "diviso";
-			break;
-		case "^" :
-			op = "alla";
-			break;
-		default:
-			op="OPERATORE NON RICONOSCIUTO";
+	curs = "(" + sleft + " " + sright + " " +  e.myOp() + " " + ")";
 	}
-	curs = "(" + sleft + " "  +  op + " " + sright + " "+ ")";
-	}
-	
-	
 	public void visit( PlusExp e ) { visitOpExp(e); }
 	public void visit( MinusExp e ) { visitOpExp(e); }
 	public void visit( TimesExp e ) { visitOpExp(e); }
 	public void visit( DivExp e ) { visitOpExp(e); }
 	public void visit( PotExp e ) { visitOpExp(e); }
-	
-	public void visit( NumExp e ) { 
-		ValueConverters intConverter = ValueConverters.ITALIAN_INTEGER;
-		String num = intConverter.asWords(e.getValue());
-		curs = "" + num; 
-	
-	}
+	public void visit( NumExp e ) { curs = "" + e.getValue(); }
 	
 }
